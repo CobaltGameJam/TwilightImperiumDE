94d3f9caab43acc2f9f35471e069e06c27a7fe70
James Cameron Abreu <coolcam222@gmail.com>
2019-12-04 09:45:08 -0500

    Love Them Hexagons
    + New project
    + Import Hexagon presets from the store
    + Tile prefab + script
    + Tile Generator script
    + Logic to generate hexegons at adjacent spots

---
+++ Twilight Imperium DE/Assets/Map/Tile/Tile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    public double GetParallelWidth()
+    {
+        MeshFilter filter = GetComponent<MeshFilter>();
+        var bounds = filter.mesh.bounds;
+        double pWidth = bounds.max.x - bounds.min.x;
+        return pWidth;
+    }
+
+    public double GetPointWidth()
+    {
+        MeshFilter filter = GetComponent<MeshFilter>();
+        var bounds = filter.mesh.bounds;
+        double pWidth = bounds.max.z - bounds.min.z;
+        return pWidth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Twilight Imperium DE/Assets/Map/Tile/TileGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+
+    public Tile tileObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    public void GenerateTiles()
+    {
+        Vector3 startingPos = new Vector3(0, 0, 0);
+
+        Tile centerTile = GameObject.Instantiate(tileObject);
+        centerTile.transform.position = startingPos;
+        double hexWidth = centerTile.GetParallelWidth() / 2;
+        double hexHeight = centerTile.GetPointWidth() * 0.75;
+
+        Debug.Log($"hexWidth = {hexWidth}");
+        Debug.Log($"hexHeight = {hexHeight}");
+
+        Tile testTile = GameObject.Instantiate(tileObject);
+        testTile.transform.position = new Vector3(startingPos.x + (float)hexWidth, startingPos.y + (float)hexHeight, 0);
+
+        //for (int i = 0; i < 5; i++)
+        //{
+
+        //}
+    }
+
+
+    public void Awake()
+    {
+        GenerateTiles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
